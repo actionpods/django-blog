74e0ca6d719fc651f7fbfbf5a4d929c4b92ef1d1
Braden Jageman <bjageman@gmail.com>
2016-12-14 17:42:35 -0800

    arg

--- action-blog/api_view.py
+++
@@ -1,8 +0,0 @@
-from serializers import *
-from models import *
-from rest_framework import viewsets
-
-#Profile/User
-class BlogViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
--- action-blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from blog import views
-
-urlpatterns = [
-            url(r'^$', views.index, name="index"),
-            url(r'^view/(?P<slug>[^\.]+)/$', views.view_post, name='view_blog_post'),
-            url(r'^category/(?P<category>[^\.]+)/$', views.view_category, name='view_blog_category'),
-            ]
